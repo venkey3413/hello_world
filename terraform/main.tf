fce4711f0fe763fb9087072c3de59822c96d29ea
venkey3413 <144769456+venkey3413@users.noreply.github.com>
2024-06-14 16:04:41 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,15 +52,33 @@ data "aws_iam_policy_document" "assume_role_policy" {
   }
 }
 
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = "ecsTaskExecutionRole"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+resource "aws_iam_role" "my_ecs_task_execution_role" {
+  name = "myEcsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
+  ]
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_roles.ecs_task_execution_role.name
+
+resource "aws_iam_role_policy_attachment" "my_ecs_task_execution_role_policy" {
+  role       = aws_iam_role.my_ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
 
 variable "docker_image" {
   description = "Docker image name"
