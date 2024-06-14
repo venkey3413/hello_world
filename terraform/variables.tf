3ff4a92fc22595d092b2483ef205e766d62bb340
venkey3413 <144769456+venkey3413@users.noreply.github.com>
2024-06-14 13:46:11 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  description = "The AWS region to deploy to"
+  default     = "us-east-1"
+}
+
+variable "role_arn" {
+  description = "The role to assume"
+  type        = string
+}
+
+variable "docker_image" {
+  description = "The Docker image for the ECS task"
+  type        = string
+}
+
+variable "account_id" {
+  description = "958955696306"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "subnet-0c2db15cd6f86cbc4", "subnet-09423f07e8dd42458"
+  type        = list(string)
+}
+
+variable "security_group_ids" {
+  description = "sg-096b3e9c824a7ba70"
+  type        = list(string)
+}
